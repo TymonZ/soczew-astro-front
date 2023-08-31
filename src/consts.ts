dfd1354058a63ab8317d97148934785c26334d7d
pxltr <tymon.zaniewski.pl@gmail.com>
2023-08-31 10:56:55 +0200

    added a const `WEBSITE_TITLE` to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
+export const WEBSITE_TITLE = 's0czew'
 export const EVENTS = [
     // {
     //     id: 0,
