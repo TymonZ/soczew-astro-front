ae655a18b28af228046f05e3fc0ee0cc70bc2916
pxltr <tymon.zaniewski.pl@gmail.com>
2023-12-14 17:34:14 +0100

    adjusted header padding size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = ({ children }) => {
 	return (
-		<h1 className='text-4xl sm:text-6xl font-body font-normal px-6 py-5 w-fit my-link-style'>
+		<h1 className='text-4xl sm:text-6xl font-body font-normal px-[25px] py-5 w-fit my-link-style'>
 			{children}
 		</h1>
 	);
