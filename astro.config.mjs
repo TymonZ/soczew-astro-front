d1237602f2c005762ec807a8f3f4931268523261
pxltr <tymon.zaniewski.pl@gmail.com>
2024-01-15 23:52:46 +0100

    added SEO plugin integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  site: 'https://soczew.org',
+  integrations: [mdx(), sitemap(), react(), tailwind(), compress()]
 });
